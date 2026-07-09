f07401b688143daaa119a59ff95970efc2cbfc60
jacquestshilonda3 <jacquestshilonda3@gmail.com>
2026-07-09 21:47:24 +0200

    Add homepage with design

--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,11 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from .models import Video
 
 def api_videos(request):
-    videos = Video.objects.all().values('id', 'titre', 'publie', 'video_url')
+    videos = Video.objects.all().values('id', 'titre', 'description', 'video_url', 'publie')
     return JsonResponse(list(videos), safe=False)
+
+def home(request):
+    videos = Video.objects.all().order_by('-publie')
+    return render(request, 'videos/home.html', {'videos': videos})
